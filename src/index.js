fa767aa77cd422f56737244d340ce3a47ef3f869
wonaval <wonaval@gmail.com>
2022-01-13 00:01:05 -0500

    Added react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+import './index.css';
+import { UserProvider } from './global/UserContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
