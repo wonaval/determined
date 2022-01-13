6f063411756714350253efdd9227bb2498d9125f
wonaval <wonaval@gmail.com>
2022-01-13 00:01:23 -0500

    Account pages WIP

--- src/Account/AccountEdit.js
+++ src/Account/AccountEdit.js
@@ -1,4 +1,87 @@
+// Import Modules
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import env from 'react-dotenv';
+
+// Import useContext
+import { UserContext } from '../global/UserContext';
+
 const AccountEdit = () => {
-  return <div>AccountEdit</div>;
+  // useContext
+  const { userState } = useContext(UserContext);
+  const [user, setUser] = userState;
+
+  // useState
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Component Functions
+  const submitForm = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${env.REACT_APP_BACKEND_URL}/user`, {
+        name: name,
+        username: username,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        localStorage.setItem('user_id', response.data.user_id);
+        setUser(response.data.user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <div>
+      <div>My Account Information</div>
+      <div>
+        <div>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          Username:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          Email:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          Password:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <input type="button" value="Edit Account" />
+        </div>
+        <div>
+          <input type="button" value="Delete Account" />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default AccountEdit;
--- src/Account/AccountView.js
+++ src/Account/AccountView.js
@@ -1,4 +1,22 @@
 const AccountView = () => {
-  return <div>AccountView</div>;
+  return (
+    <div>
+      <div>My Account Information</div>
+      <div>
+        <div>Name:</div>
+        <div>Username:</div>
+        <div>Email:</div>
+        <div>Password: ***</div>
+      </div>
+      <div>
+        <div>
+          <input type="button" value="Edit Account" />
+        </div>
+        <div>
+          <input type="button" value="Delete Account" />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default AccountView;
